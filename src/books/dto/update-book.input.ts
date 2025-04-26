7e2ee738e93294d72d3b78d0b13e2d2afb9773cf
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2025-04-26 23:41:20 +0300

    update book validations

--- src/books/dto/update-book.input.ts
+++ src/books/dto/update-book.input.ts
@@ -1,13 +1,27 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
-
+import {
+  IsOptional,
+  Length,
+  IsNotEmpty,
+  IsPositive,
+  IsInt,
+} from 'class-validator';
 @InputType()
 export class UpdateBookInput {
+  @IsOptional()
+  @IsNotEmpty()
+  @Length(3)
   @Field({ nullable: true })
   title?: string;
 
+  @IsOptional()
+  @IsNotEmpty()
   @Field({ nullable: true })
   author?: string;
 
+  @IsNotEmpty()
+  @IsPositive()
+  @IsInt()
   @Field(() => Int)
   id: number;
 }
