3d3c98825c65a3f4d018fffea45bdb206018e4e5
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2025-04-26 14:40:14 +0300

    functions moved from resolver to service

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -2,85 +2,43 @@ import { Resolver, Query, Args, Mutation, Int } from '@nestjs/graphql';
 import { Book } from './entitites/book.entity';
 import { CreateBookInput } from './dto/create-book.input';
 import { UpdateBookInput } from './dto/update-book.input';
+import { BooksService } from './books.service';
 @Resolver(() => Book)
 export class BooksResolver {
-  private books: Book[] = [
-    { id: 1, title: 'Hello There', author: 'Obi Wan Kenobi' },
-    { id: 2, title: 'Somebody To Love', author: 'Freddie' },
-  ];
+  constructor(private readonly booksService: BooksService) {}
 
   @Query(() => [Book])
   getBooks(): Book[] {
-    return this.books;
+    return this.booksService.getBooks();
   }
 
   @Query(() => [Book])
   getBooksByAuthor(@Args('author') author: string): Book[] {
-    return this.getBooks().filter((b) => b.author === author);
+    return this.booksService.getBooksByAuthor(author);
   }
 
   @Query(() => Book)
   getBookById(@Args('id') id: number): Book {
-    const book = this.getBooks().find((book) => book.id === id);
-
-    if (!book) {
-      throw new Error('No book with given id');
-    }
-
-    return book;
+    return this.booksService.getBookById(id);
   }
 
   @Query(() => Int)
   countBooks(): number {
-    return this.books.length;
+    return this.booksService.countBooks();
   }
 
   @Mutation(() => Book)
   createBook(@Args('createBookInput') createBookInput: CreateBookInput): Book {
-    const { title, author } = createBookInput;
-    const lastId =
-      this.books.length > 0 ? this.books[this.books.length - 1].id : 0;
-
-    const book: Book = {
-      title,
-      author,
-      id: lastId,
-    };
-
-    this.books.push(book);
-
-    return book;
+    return this.booksService.createBook(createBookInput);
   }
 
   @Mutation(() => Book)
   updateBook(@Args('updateBookInput') updateBookInput: UpdateBookInput): Book {
-    const { id, title, author } = updateBookInput;
-
-    const book = this.books.find((book) => book.id === id);
-
-    if (!book) {
-      throw new Error(`Book with id ${id} not found`);
-    }
-
-    if (title) {
-      book.title = title;
-    } else if (author) {
-      book.author = author;
-    }
-
-    return book;
+    return this.booksService.updateBook(updateBookInput);
   }
 
   @Mutation(() => Boolean)
   deleteBook(@Args('id') id: number): boolean {
-    const bookIndex = this.books.findIndex((book) => book.id === id);
-
-    if (bookIndex === -1) {
-      throw new Error(`Book with id ${id} not found`);
-    }
-
-    this.books.splice(bookIndex, 1);
-
-    return true;
+    return this.booksService.deleteBook(id);
   }
 }
---
+++ src/books/books.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@nestjs/common';
+import { Book } from './entitites/book.entity';
+import { CreateBookInput } from './dto/create-book.input';
+import { Args, Int, Mutation, Query } from '@nestjs/graphql';
+import { UpdateBookInput } from './dto/update-book.input';
+
+@Injectable()
+export class BooksService {
+  private books: Book[] = [
+    { id: 1, title: 'Hello There', author: 'Obi Wan Kenobi' },
+    { id: 2, title: 'Somebody To Love', author: 'Freddie' },
+  ];
+
+  @Query(() => [Book])
+  getBooks(): Book[] {
+    return this.books;
+  }
+
+  @Query(() => [Book])
+  getBooksByAuthor(@Args('author') author: string): Book[] {
+    return this.getBooks().filter((b) => b.author === author);
+  }
+
+  @Query(() => Book)
+  getBookById(@Args('id') id: number): Book {
+    const book = this.getBooks().find((book) => book.id === id);
+
+    if (!book) {
+      throw new Error('No book with given id');
+    }
+
+    return book;
+  }
+
+  @Query(() => Int)
+  countBooks(): number {
+    return this.books.length;
+  }
+
+  @Mutation(() => Book)
+  createBook(@Args('createBookInput') createBookInput: CreateBookInput): Book {
+    const { title, author } = createBookInput;
+    const lastId =
+      this.books.length > 0 ? this.books[this.books.length - 1].id : 0;
+
+    const book: Book = {
+      title,
+      author,
+      id: lastId,
+    };
+
+    this.books.push(book);
+
+    return book;
+  }
+
+  @Mutation(() => Book)
+  updateBook(@Args('updateBookInput') updateBookInput: UpdateBookInput): Book {
+    const { id, title, author } = updateBookInput;
+
+    const book = this.books.find((book) => book.id === id);
+
+    if (!book) {
+      throw new Error(`Book with id ${id} not found`);
+    }
+
+    if (title) {
+      book.title = title;
+    } else if (author) {
+      book.author = author;
+    }
+
+    return book;
+  }
+
+  @Mutation(() => Boolean)
+  deleteBook(@Args('id') id: number): boolean {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+
+    if (bookIndex === -1) {
+      throw new Error(`Book with id ${id} not found`);
+    }
+
+    this.books.splice(bookIndex, 1);
+
+    return true;
+  }
+}
