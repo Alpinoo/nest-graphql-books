5c25e09696078da4a9fa8eb7e7f7db0a65f8f0db
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2025-04-26 23:36:53 +0300

    create book validation

--- src/books/dto/create-book.input.ts
+++ src/books/dto/create-book.input.ts
@@ -1,10 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
-
+import { IsNotEmpty, Length } from 'class-validator';
 @InputType()
 export class CreateBookInput {
+  @IsNotEmpty()
   @Field()
   author: string;
 
+  @IsNotEmpty()
+  @Length(3)
   @Field()
   title: string;
 }
